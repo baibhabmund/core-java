946a789093501d409e62fbf702a7dff0407da2fc
baibhabmund <baibhab.mund20@gmail.com>
2025-01-17 09:30:06 +0530

    next change

---
+++ src/decimalToBinary.java
@@ -0,0 +1,12 @@
+/*import java.util.Scanner;
+public class decimalToBinary {
+    public static void main{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your number : ");
+        int a = sc.nextInt();
+
+
+    }
+}
+*/
