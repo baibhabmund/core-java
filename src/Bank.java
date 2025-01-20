074e6a2983138b0f3dbdf6a6361a8e28ce8412e8
baibhabmund <baibhab.mund20@gmail.com>
2025-01-20 15:44:31 +0530

    Abstract Class Bank

---
+++ src/Bank.java
@@ -0,0 +1,37 @@
+abstract class Bank {
+    abstract void getBlance();
+    public static void main(String args[]){
+        BankA bankA = new BankA();
+        BankB bankB = new BankB();
+        BankC bankC = new BankC();
+
+        bankA.getBlance();
+        bankB.getBlance();
+        bankC.getBlance();
+
+    }
+}
+
+class BankA extends Bank{
+    private double balance = 100.00;
+    @Override
+    void getBlance() {
+        System.out.println("Your current balance is $" + balance);
+    }
+}
+class BankB extends Bank{
+    private double balance = 150.00;
+    @Override
+    void getBlance() {
+        System.out.println("Your current balance is $" + balance);
+    }
+}
+class BankC extends Bank{
+    private double balance = 200.00;
+    @Override
+    void getBlance() {
+        System.out.println("Your current balance is $" + balance);
+    }
+}
+
+
