12ea982c7bb513e384861b94cc16f61d7ea45742
baibhabmund <baibhab.mund20@gmail.com>
2025-01-20 11:05:26 +0530

    Implementing Interface

--- src/Playable.java
+++ src/Playable.java
@@ -1,17 +1,26 @@
 public interface Playable {
     public void play();
+    default void stop(){
+        System.out.println("Stop the Audio.");
+    }
     public static void main(String args[]){
         Playable videoplayer = new videoPlayer();
         Playable audioplayer = new audioPlayer();
 
         videoplayer.play();
         audioplayer.play();
+
+        videoplayer.stop();
+        audioplayer.stop();
     }
 }
 class videoPlayer implements Playable{
     @Override
     public void play(){
         System.out.println("Play Video");
+    }
+    public void stop(){
+        System.out.println("Stoping the Video...");
     }
 }
 class audioPlayer implements Playable{
