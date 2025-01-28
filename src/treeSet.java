187e878ad431056d29feb4cc9b05b637336aac6f
baibhabmund <baibhab.mund20@gmail.com>
2025-01-28 09:49:06 +0530

    Tree Set

---
+++ src/treeSet.java
@@ -0,0 +1,35 @@
+import com.sun.source.tree.Tree;
+import com.sun.source.util.Trees;
+
+import java.util.TreeSet;
+public class treeSet {
+    public static void main(String[] args) {
+
+        /*
+        TreeSet <Integer> TreeSet = new TreeSet <Integer>();
+        TreeSet.add(50);
+        TreeSet.add(20);
+        TreeSet.add(40);
+       */
+
+        /*PRINT FIRST AND LAST ELEMENT
+
+        TreeSet <String> TreeSet = new TreeSet <String>();
+        TreeSet.add("Zebra");
+        TreeSet.add("Apple");
+        TreeSet.add("Mango");
+
+        System.out.println("First Element = " + TreeSet.first());
+        System.out.println("First Element = " + TreeSet.last());
+         */
+
+        /* REMOVE THE SMALLEST ELEMENT
+        TreeSet <Integer> TreeSet = new TreeSet <Integer>();
+        TreeSet.add(5);
+        TreeSet.add(10);
+        TreeSet.add(15);
+        int r_ele = TreeSet.pollFirst();
+        System.out.println(TreeSet);
+         */
+    }
+}
