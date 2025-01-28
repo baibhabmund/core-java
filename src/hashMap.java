f5ff23ae558bd2b1d77a19c0a519b5d0b92f65a6
baibhabmund <baibhab.mund20@gmail.com>
2025-01-28 11:38:23 +0530

    Hash Map

---
+++ src/hashMap.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+public class hashMap {
+    public static void main(String[] args) {
+        /*HashMap<String, Integer> map = new HashMap<>();
+
+        //Adding the elements
+        map.put("Alice", 70);
+        map.put("Bob", 85);
+        map.put("John Doe", 73);
+        map.put("Charlie", 59);
+
+        //Retrieving the value
+        System.out.println("Value for Key : " + map.get("Bob"));
+
+        //Iterating through the map
+        for(String name_Key : map.keySet()){
+            System.out.println("Key(Name) : " + name_Key + " , Value(Marks) : " + map.get(name_Key));
+        }
+
+        //Removing an Element
+        map.remove("John Doe");
+        System.out.println("Updated one : " + map);
+         */
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("India", "New Delhi");
+        map.put("USA", "Washington");
+        map.put("Pakistan", "Islamabad");
+        map.put("Russia", "Moscow");
+
+
+    }
+}
